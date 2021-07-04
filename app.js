7537e284ed83f26e46aef1508f1bc8f12d3c2266
Lixian <weiyuanleeif@gmail.com>
2021-07-04 14:19:55 -0400

    dice image array, dice function made

---
+++ app.js
@@ -0,0 +1,20 @@
+const compDice = document.getElementById("dice")
+const compCards = document.getElementById("card-deck")
+const rollBtn = document.getElementById("roll")
+const drawBtn = document.getElementById("draw")
+
+let diceGame.addEventListner("click") {
+
+}
+
+function diceRollRandom() {
+    const diceImages = ["dice1.svg", "dice2.svg", "dice3.svg", "dice4.svg", "dice5.svg", "dice6.svg"]
+    diceImages[0] = "dice1.svg"
+    diceImages[1] = "dice2.svg"
+    diceImages[2] = "dice3.svg"
+    diceImages[3] = "dice4.svg"
+    diceImages[4] = "dice5.svg" 
+    diceImages[5] = "dice6.svg"
+
+
+}
